ad5ee5e70d632a6ba622f3e918886c4de52f5b99
Ekta Prasad <ektaprasad3582@gmail.com>
2021-06-29 19:09:55 +0530

    hashed password

--- demo_express/routes/details.js
+++ demo_express/routes/details.js
@@ -2,6 +2,7 @@ const express = require ('express') //importing the express
 // const empDetails = require('../../modles/empDetails')
 const router = express. Router() //importing routrer function
 const myModel = require('../modles/empDetails')
+const bcrypt = require('bcrypt')
 const jwt= require('jsonwebtoken')
 const verifyToken = require('./verify')
 router.get('/details', (req, res) => {
@@ -24,10 +25,15 @@ const token=jwt.sign({_id:123456},'ekta')
     module.exports = router //exporting routes to use
   
     router.post('/details', async(req, res) => {
+      const salt = await bcrypt.genSalt(10) //generating salt
+
+      const hashPassword = await bcrypt.hash(req.body.password,salt) //hashing a password
+    
       const details = new myModel({
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password
+      //  password:req.body.password  password without hashing
+        password:hashPassword // gives hash password
       })
      // res.send(details) to send response. it will retrive data from body.
     // details.save();//to only send and save data in db when we send it from body but we will not able to see the response
